c331b52d0821d1f20f3dd7170e30e8b37412c208
Felipe Rodrigues <felipe13rodrigues.fr@gmail.com>
2020-09-09 22:56:10 -0300

    Criei uma biblioteca

---
+++ ConsoleBank/ConsoleBank.SistemaAgencia/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleBank.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente(123, 550017);
+
+            Console.ReadLine();
+        }
+    }
+}
